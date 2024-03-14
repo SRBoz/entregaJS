9d40c74ea4c3848bc6865675403af47f692f6a82
SRBoz <srolandoboz@gmail.com>
2024-03-13 21:44:43 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+let numero = prompt()
+function esPar(numero) {
+    if (numero % 2 === 0) {
+        return true;
+        // console.log("El numero " + numero + " es par");
+    } else {
+        return false;
+        // console.log("El numero " + numero + " es impar");
+    }
+
+
+}
+
+for (let i = 1; i <= numero; i++) {
+    if (esPar(i)) {
+        console.log("El numero " + i + " es par")
+    } else {
+        console.log("El numero " + i + " es impar")
+    }
+
+}
